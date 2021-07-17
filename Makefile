681f937c36f37215dfc3f2e1a2cf0d1a6a8cf893
mharriso <mharriso@student.21-school.ru>
2021-07-17 13:50:13 +0300

    delete

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 NAME	=	pipex
-NAME_BONUS = pipex_bonus
 SRCDIR	=	src
 OBJDIR	=	.obj
 FT		=	libft
@@ -13,15 +12,10 @@ SRCS	= main.c \
 		  split_path.c \
 		  execute.c \
 		  pipex_handler.c
-SRCS_BONUS	= main_bonus.c \
-			  utils.c \
-			  split_path.c \
-			  execute.c \
-			  pipex_handler.c
+
 SRCS	:=	$(addprefix $(SRCDIR)/, $(SRCS))
-FLAGS	=	-g -Wall -Wextra -Werror
-OBJS	=	$(SRCS:$(SRCDIR)/%.c=$(OBJDIR)/%.o)
-OBJS_BONUS	=	$(SRCS_BONUS:$(SRCDIR)/%.c=$(OBJDIR)/%.o)
+FLAGS	=	-Wall -Wextra -Werror
+OBJECTS	=	$(SRCS:$(SRCDIR)/%.c=$(OBJDIR)/%.o)
 
 GREEN      = [1;32m
 RESET      = [0m
@@ -30,7 +24,7 @@ all: ${NAME}
 
 $(NAME): ${OBJECTS}
 	make bonus -C ${FT}
-	gcc -I ${INC} $(OBJS) -l ft -L ${FT} -o $(NAME)
+	gcc -I ${INC} $(OBJECTS) -l ft -L ${FT} -o $(NAME)
 	@echo "$(GREEN)Built target $(NAME)$(RESET)"
 $(OBJDIR)/%.o: $(SRCDIR)/%.c | $(OBJDIR)
 	gcc ${FLAGS} -I ${INC} -c $< -o $@
@@ -45,16 +39,7 @@ fclean: clean
 	rm -f ${NAME}
 re: fclean all
 
-bonus: ${NAME_BONUS}
-
-$(NAME_BONUS): ${OBJECTS}
-	make bonus -C ${FT}
-	gcc -I ${INC} $(OBJS_BONUS) -l ft -L ${FT} -o $(NAME_BONUS)
-	@echo "$(GREEN)Built target $(NAME_BONUS)$(RESET)"
-$(OBJDIR)/%.o: $(SRCDIR)/%.c | $(OBJDIR)
-	gcc ${FLAGS} -I ${INC} -c $< -o $@
-$(OBJDIR):
-	@mkdir -p ${NAME_BONUS}
+bonus: ${NAME}
 
 norm:
 	norminette src/* inc/*
--- src/main.c
+++ src/main.c
@@ -1,18 +1,56 @@
 #include "pipex.h"
 
+static t_cmd	*init_cmd(char **argv, size_t size)
+{
+	t_cmd		*cmd;
+	size_t		i;
+
+	cmd = malloc(size * sizeof(t_cmd));
+	if (!cmd)
+		exit_error("Can not allocate cmd");
+	i = 0;
+	while (i < size)
+	{
+		cmd[i].arg = ft_split(argv[i + 2], ' ');
+		if (!cmd[i].arg)
+			exit_error("Can not allocate cmd arguments");
+		if (i == 0)
+			cmd[i].type = R_LEFT;
+		else if (i == size - 1)
+			cmd[i].type = R_RIGHT;
+		else
+			cmd[i].type = PIPE;
+		i++;
+	}
+	return (cmd);
+}
+
 static void	save_args(t_info *info, int argc, char **argv)
 {
-	if (argc != 5)
-		exit_error("Invalid arguments: file1 cmd1 cmd2 file2");
-	info->limiter = NULL;
-	info->size = 2;
-	info->file_in = argv[1];
-	info->file_out = argv[4];
-	info->cmd = malloc(2 * sizeof(t_cmd));
-	info->cmd[0].arg = ft_split(argv[2], ' ');
-	info->cmd[0].type = R_LEFT;
-	info->cmd[1].arg = ft_split(argv[3], ' ');
-	info->cmd[1].type = R_RIGHT;
+	if (argc > 1 && !ft_strcmp(argv[1], "here_doc"))
+	{
+		if (argc != 6)
+			exit_error("Invalid arguments: here_doc LIMITER cmd cmd1 file");
+		info->limiter = argv[2];
+		info->size = 2;
+		info->cmd = malloc(2 * sizeof(t_cmd));
+		info->cmd[0].arg = ft_split(argv[3], ' ');
+		info->cmd[0].type = R_DLEFT;
+		info->cmd[1].arg = ft_split(argv[4], ' ');
+		info->cmd[1].type = R_DRIGHT;
+		info->file_in = argv[1];
+		info->file_out = argv[5];
+	}
+	else
+	{
+		if (argc < 5)
+			exit_error("Invalid arguments");
+		info->limiter = NULL;
+		info->size = argc - 3;
+		info->file_in = argv[1];
+		info->file_out = argv[argc - 1];
+		info->cmd = init_cmd(argv, info->size);
+	}
 }
 
 static void	free_info(t_info *info)
@@ -39,8 +77,8 @@ int	main(int argc, char const **argv, char **env)
 		exit_error("Can not allocate info");
 	save_args(info, argc, (char **)argv);
 	execute_commands(info, env);
-	free_info(info);
 	if (info->limiter)
 		unlink("here_doc");
+	free_info(info);
 	return (0);
 }
